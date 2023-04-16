51bbad49c28b2e031c139dde0af5d410ad7e8e67
Deepak Singh <deepkushwah824@gmail.com>
2023-04-16 13:12:26 +0530

    date added and css fixe,roadname update

--- src/App.js
+++ src/App.js
@@ -2,38 +2,44 @@ import { useJsApiLoader } from "@react-google-maps/api";
 import Map from "./components/Map";
 import { mapOptions } from "./components/MapConfiguration";
 import SideBar from "./components/SideBar";
-import "./App.css"
+import "./App.css";
 import { useState } from "react";
 
 function App() {
   const { isLoaded } = useJsApiLoader({
-    id:mapOptions.googleMapApiKey,
-    googleMapsApiKey:mapOptions.googleMapApiKey,
+    id: mapOptions.googleMapApiKey,
+    googleMapsApiKey: mapOptions.googleMapApiKey,
   });
-  const [trip,setTrip] = useState('');
-  const [dist,setDist] = useState();
-  function getTrip(trip){
+  const [trip, setTrip] = useState("");
+  const [dist, setDist] = useState();
+  function getTrip(trip) {
     setTrip(trip);
   }
-  function getDist(d){
+  function getDist(d) {
     setDist(d);
   }
-  const [clear,isClear] = useState(0);
-  function setClear(cr){
-    
+  const [clear, isClear] = useState(0);
+  function setClear(cr) {
     isClear(1);
-   
   }
   return (
-    
     <div className="App">
-      <SideBar className="sidebar" trip={trip} setClear={setClear} dist={dist}/>
+      <SideBar
+        className="sidebar"
+        trip={trip}
+        setClear={setClear}
+        dist={dist}
+      />
       {/* Loading of map Main code in map.js */}
 
-      <Map  className="map" isLoaded={isLoaded} getTrip={getTrip} Clear={clear} getDist = {getDist}/>
-
+      <Map
+        className="map"
+        isLoaded={isLoaded}
+        getTrip={getTrip}
+        Clear={clear}
+        getDist={getDist}
+      />
     </div>
-    
   );
 }
 
